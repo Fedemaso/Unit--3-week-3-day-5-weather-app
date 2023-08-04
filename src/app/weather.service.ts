52bce241886d1556cd3285137eb6d90af6aade29
fedemaso <fede.maso1989@gmail.com>
2023-08-04 17:50:30 +0200

    asd

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -8,24 +8,24 @@ import { Observable, of } from 'rxjs';
 })
 export class WeatherService {
   private apiUrl = 'http://api.openweathermap.org/data/2.5/forecast';
-  private apiKey = ;
+  private apiKey = '';
 
   constructor(private http: HttpClient) { }
 
   getWeatherForecasts(): Observable<any> {
     const lat = 'YOUR_LATITUDE';
     const lon = 'YOUR_LONGITUDE';
-    const url = `${this.apiUrl}?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
+    const url = `${this.apiUrl}?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
     return this.http.get(url);
   }
 
   getWeatherByCityName(cityName: string, stateCode: string, countryCode: string) {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateCode},${countryCode}&appid=${this.apiKey}&units=metric`;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateCode},${countryCode}&appid=${this.apiKey}`;
     return this.http.get(url);
   }
 
   getCoordinates(cityName: string) {
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${this.apiKey}&units=metric`;
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${this.apiKey}`;
     return this.http.get<any[]>(url);
   }
 
