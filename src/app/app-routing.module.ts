ead3c70b0f0afcfaa03e2589f94abaf01a6a3fd4
fedemaso <fede.maso1989@gmail.com>
2023-08-04 12:48:19 +0200

    Unità 3 Week 3 Day 5 Weather-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { WeatherListComponent } from './weather-list/weather-list.component';
+import { CitySearchComponent } from './city-search/city-search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WeatherListComponent },
+  { path: 'search', component: CitySearchComponent },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherListComponent } from './weather-list/weather-list.component';
+import { CitySearchComponent } from './city-search/city-search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherListComponent,
+    CitySearchComponent,
+    FavoritesComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
